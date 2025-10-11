2722cb91f9e670c67ae42f78111885389f9a14aa
Mazen <mazen.elgharib@ncsc.gov.bh>
2025-10-11 21:22:09 +0300

    Add production frontend build, ready to deploy

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
 let morgan = require('morgan');
+dotenv.config();
 const app = express();
-const PORT = 3001;
+
+app.use(express.static("dist"))
 app.use(express.json());
+
+// Enable CORS for all origins (for development purposes)
+// app.use(cors({
+//     origin: (origin,callback)=>{
+//         if(!origin || origin === process.env.CLIENT_URL){
+//            return callback(null,true);
+//         }
+//         return callback(new Error("Not allowed by CORS"))
+
+//     }
+// }))
+
+
 morgan.token('body', (req,res) => req.body? JSON.stringify(req.body) :'')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -77,7 +95,25 @@ app.post("/api/persons",(req,res)=>{
     return res.status(201).send(newPerson)
 })
 
+app.put("/api/persons/:id",(req,res)=>{
+    console.log(`req body : ${JSON.stringify(req.body)}
+    params : ${JSON.stringify(req.params)}`);
+    const{id} = req.params;
+    const{number} = req.body || {};
+    const person = persons.find(p => p.id === id)
+    if (!person){
+        return res.status(404).send("Person not found")
+    }
+    person.number = number ?? person.number;
 
+    return res.send(person)
+})
+
+app.use((req,res)=>{
+    return res.sendFile(path.resolve(__dirname ,"dist","index.html"))
+})
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 })
